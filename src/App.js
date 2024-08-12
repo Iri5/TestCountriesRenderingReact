2d9751d94f343a0dfe2b7ebdc2149349841a80e0
Iri5 <bovchurova-irina@rambler.ru>
2024-08-12 14:32:50 +0300

    add preloader

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useParams, NavLink} from "react-router-dom";
+import { Routes, Route, useParams, NavLink } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import loader from './loader.gif';
+import './loader.css';
 function App() {
   return (
-      <Routes className="list-group">
-        <Route path='/' element={<CountriesList/>}></Route>
-        <Route path='/:name' element={<CountryCard/>}></Route>
-      </Routes>
+    <Routes className="list-group">
+      <Route path='/' element={<CountriesList />}></Route>
+      <Route path='/:name' element={<CountryCard />}></Route>
+    </Routes>
   );
 }
 function CountriesList() {
   const countryURL = 'https://restcountries.com/v3.1/all';
 
   const [countries, setCountries] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [status, setStatus] = useState(0);
 
   useEffect(() => {
     fetch(countryURL)
       .then((res) => {
+        setStatus(res.status);
         return res.json();
       })
       .then((data) => {
+        setIsFetching(false);
         setCountries(data);
       });
   }, []);
+  if ((status != 200) && !isFetching) return <h2>Error</h2>
   return (
     <div className="container-fluid text-left">
       <h1>Countries</h1>
-
+      {isFetching ? <Preloader /> : null}
       <ul className="list-group">
         {countries &&
           countries.map((country, index) => {
@@ -49,32 +55,40 @@ function CountryCard() {
   const params = useParams();
   const oneCountryURL = `https://restcountries.com/v3.1/name/${params.name}`;
   const [country, setCountry] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [status, setStatus] = useState(0);
+
   useEffect(() => {
     fetch(oneCountryURL)
       .then((res) => {
+        setStatus(res.status);
         return res.json();
       })
       .then((data) => {
+        setIsFetching(false);
         setCountry(data);
+        console.log(status);
       });
   }, []);
+  if ((status != 200) && !isFetching) return <h2>Error</h2>
   if (country) {
     return (
-      <div className='card text-bg-info' style={{ width: 30 + '%' }}>
-        <img className='card-img-top' src={country[0].flags.png} alt={'The flag of' + country[0].name.official}></img>
-        <div>
-          <h2 className='card-title'>{country[0].name.official}</h2>
-          <p>Status: {country[0].status}</p>
-          <p>Region: {country[0].region}</p>
-          <p>Capital: {country[0].capital}</p>
+      <div className="container-fluid">
+        {isFetching ? <Preloader /> : null}
+        <div className='card text-bg-info' style={{ width: 30 + '%' }}>
+          <img className='card-img-top' src={country[0].flags.png} alt={'The flag of' + country[0].name.official}></img>
+          <div>
+            <h2 className='card-title'>{country[0].name.official}</h2>
+            <p>Status: {country[0].status}</p>
+            <p>Region: {country[0].region}</p>
+            <p>Capital: {country[0].capital}</p>
+          </div>
         </div>
       </div>
     )
   }
-  else {
-    return <h2>Error</h2>
-  }
-
 }
-
+const Preloader = () => {
+  return <img src={loader} className="loader" alt="preloader" />
+}
 export default App;
